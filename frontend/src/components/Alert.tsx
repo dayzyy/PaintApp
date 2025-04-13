bf58ab632b85d2ce559d454e465f7f0de7ce94d5
Luka Mania <manialuka19@gmail.com>
2025-04-14 00:10:50 +0400

    feat(frontend) redesign the alert

--- frontend/src/components/Alert.tsx
+++ frontend/src/components/Alert.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, ReactNode, useEffect, useState } from "react"
+import { ReactNode, useEffect, useState } from "react"
 
 import { IoCloseOutline } from "react-icons/io5";
 
@@ -6,8 +6,8 @@ type AlertProps = {
     icon: ReactNode
     text: string
     info: string
-    toggle_off: MouseEventHandler<HTMLDivElement>
-    disable: MouseEventHandler<HTMLDivElement>
+    toggle_off: () => void
+    disable: () => void
 }
 
 const Alert = ({icon, text, info, toggle_off, disable}: AlertProps) => {
@@ -35,36 +35,38 @@ const Alert = ({icon, text, info, toggle_off, disable}: AlertProps) => {
 
     return (
 	<div 
-	    className={`fixed top-5  w-[80%]  bg-[var(--color-bg-alert)]  rounded-md  flex flex-col p-2 z-[1122]
+	    className={`fixed top-5  w-[80%]  rounded-md  flex flex-col  gap-4  z-[1122]
 		md:max-w-[800px] md:h-[5rem] md:flex-row md:items-center
 		transition-[translate] duration-200
 		[&_*]:select-none
 	 	${show ? 'translate-y-0' : '-translate-y-[200%]'}`}
 	>
-	    <div className="h-[4rem] md:h-full  flex-grow  flex gap-3 items-center">
-		<div>	
-		    {icon}
-		</div>
+	    <div className="h-[4rem] md:h-full  bg-[var(--color-bg-alert)]  p-2 rounded-md  flex-grow  flex items-center justify-between">
+		<div className="h-full flex items-center gap-3">
+		    <div>	
+			{icon}
+		    </div>
 
-		<div className="h-full  flex flex-col justify-around  py-1">
-		    <h1 className="text-[var(--color-text-alert-msg)] leading-0">{text}</h1>
-		    <p className="text-[var(--color-text-alert-msg)] leading-0">{info}</p>
-		</div>
-	    </div>
-
-	    <div className="h-full  flex-grow-0  flex items-center justify-around gap-4  cursor-pointer">
-		<div
-		    className="h-[70%]  px-3  border-[var(--color-border-alert-act)] border-x-2  grid place-items-center"
-		    onClick={own_disable}
-		>
-		    <h1 className="text-[var(--color-text-alert-act)]">Never show again</h1>
+		    <div className="h-full  flex flex-col justify-around  py-1">
+			<h1 className="text-[var(--color-text-alert-msg)] leading-0">{text}</h1>
+			<p className="text-[var(--color-text-alert-msg)] leading-0">{info}</p>
+		    </div>
 		</div>
 
 		<div>
 		    <IoCloseOutline
-			className="text-[3.5rem] text-[var(--color-text-alert-msg)]"
+		    className="text-[3.5rem] text-[var(--color-text-alert-msg) cursor-pointer"
 			onClick={hide}
 		    />
+		</div>
+	    </div>
+
+	    <div className="h-full w-fit  flex-grow-0 self-end  bg-[var(--color-bg-alert)]  p-2 rounded-md  flex items-center  cursor-pointer">
+		<div
+		    className="h-[70%]  px-3  grid place-items-center"
+		    onClick={own_disable}
+		>
+		    <h1 className="text-[var(--color-text-alert-act)]">Never show again</h1>
 		</div>
 	    </div>
 	</div>
